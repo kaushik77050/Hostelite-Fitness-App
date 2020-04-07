3f3ac6eb074f08d669f097e49cd3d31d5f376270
kaushik77050 <kaushik.agrawal77050@gmail.com>
2020-04-07 09:48:19 +0530

    till 13

--- app/src/main/java/com/example/myapplication/DBAdapter.java
+++ app/src/main/java/com/example/myapplication/DBAdapter.java
@@ -4,75 +4,35 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 public class DBAdapter {
-    //1.Variables
-    private static final String databaseName = "strandiet";
-    private static final int databaseVersion = 12;
+    /* 01 Variables ---------------------------------------- */
+    private static final String databaseName = "stramdiet";
+    private static final int databaseVersion = 28;
 
-    //2.Database Variables
+    /* 02 Database variables ------------------------------- */
     private final Context context;
-    private  DatabaseHelper DBHelper;
+    private DatabaseHelper DBHelper;
     private SQLiteDatabase db;
 
-    //3.Class Adapter
+
+    /* 03 Class DbAdapter ---------------------------------- */
     public DBAdapter(Context ctx){
         this.context = ctx;
         DBHelper = new DatabaseHelper(context);
     }
 
-    //4.DatabaseHelper class
-    private static class DatabaseHelper extends SQLiteOpenHelper{
+    /* 04 DatabaseHelper ------------------------------------ */
+    private static class DatabaseHelper extends SQLiteOpenHelper {
         DatabaseHelper(Context context){
-            super(context, databaseName, null,databaseVersion);
+            super(context, databaseName, null, databaseVersion);
         }
 
         @Override
-        public void onCreate(SQLiteDatabase db) {
-//            try{
-//
-//                db.execSQL("CREATE TABLE IF NOT EXISTS categories (" +
-//                        " category_id INTEGER PRIMARY KEY AUTOINCREMENT," +
-//                        " category_name VARCHAR," +
-//                        " category_parent_id INT," +
-//                        " category_icon VARCHAR," +
-//                        " category_note VARCHAR);");
-//
-//
-//                //create tables
-//                db.execSQL("CREATE TABLE IF NOT EXISTS food (" +
-//                        " food_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-//                        " food_name VARCHAR," +
-//                        " food_manufactor_name VARCHAR," +
-//                        " food_serving_size DOUBLE," +
-//                        " food_serving_mesurment VARCHAR," +
-//                        " food_serving_name_number DOUBLE," +
-//                        " food_serving_name_word VARCHAR," +
-//                        " food_energy DOUBLE," +
-//                        " food_proteins DOUBLE," +
-//                        " food_carbohydrates DOUBLE," +
-//                        " food_fat DOUBLE," +
-//                        " food_energy_calculated DOUBLE," +
-//                        " food_proteins_calculated DOUBLE," +
-//                        " food_carbohydrates_calculated DOUBLE," +
-//                        " food_fat_calculated DOUBLE," +
-//                        " food_user_id INT," +
-//                        " food_barcode VARCHAR," +
-//                        " food_category_id INT," +
-//                        " food_thumb VARCHAR," +
-//                        " food_image_a VARCHAR," +
-//                        " food_image_b VARCHAR," +
-//                        " food_image_c VARCHAR," +
-//                        " food_notes VARCHAR);");
-//
-//            }
-//            catch (SQLException e){
-//                e.printStackTrace();
-//            }
-
-
+        public void onCreate(SQLiteDatabase db){
             try{
                 // Create tables
                 db.execSQL("CREATE TABLE IF NOT EXISTS users (" +
@@ -89,6 +49,7 @@ public class DBAdapter {
                         " user_weight INT, " +
                         " user_target_weight INT, " +
                         " user_target_weight_level INT," +
+                        " user_mesurment VARHCAR, " +
                         " user_last_seen TIME," +
                         " user_note VARCHAR);");
 
@@ -178,6 +139,9 @@ public class DBAdapter {
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+
+            // ! All tables that are going to be dropped need to be listed here
+
             db.execSQL("DROP TABLE IF EXISTS users");
             db.execSQL("DROP TABLE IF EXISTS food_diary_cal_eaten");
             db.execSQL("DROP TABLE IF EXISTS food_diary");
@@ -186,33 +150,86 @@ public class DBAdapter {
             onCreate(db);
 
             String TAG = "Tag";
-            Log.w(TAG,"Upgrading database from version "+ oldVersion + " to " + newVersion+", which will destroy all old data");
-        }
-    }
+            Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
+                    + newVersion + ", which will destroy all old data");
 
-    //5.Open Database
-    public DBAdapter open() throws SQLException{
+        } // end public void onUpgrade
+    } // DatabaseHelper
+
+
+    /* 05 Open database --------------------------------------------------------- */
+    public DBAdapter open() throws SQLException {
         db = DBHelper.getWritableDatabase();
         return this;
     }
 
-    //6.Close database
-    public  void close(){
+    /* 06 Close database --------------------------------------------------------- */
+    public void close() {
         DBHelper.close();
     }
 
-    //7.Insert data
-    public void insert(String table, String fields,  String values){
-        db.execSQL("INSERT INTO " + table + " (" + fields + ") VALUES (" + values + ")");
+    /* 07 Quote smart ------------------------------------------------------------ */
+    public String quoteSmart(String value){
+        // Is numeric?
+        boolean isNumeric = false;
+        try {
+            double myDouble = Double.parseDouble(value);
+            isNumeric = true;
+        }
+        catch(NumberFormatException nfe) {
+            System.out.println("Could not parse " + nfe);
+        }
+        if(isNumeric == false){
+            // Escapes special characters in a string for use in an SQL statement
+            if (value != null && value.length() > 0) {
+                value = value.replace("\\", "\\\\");
+                value = value.replace("'", "\\'");
+                value = value.replace("\0", "\\0");
+                value = value.replace("\n", "\\n");
+                value = value.replace("\r", "\\r");
+                value = value.replace("\"", "\\\"");
+                value = value.replace("\\x1a", "\\Z");
+            }
+        }
+
+        value = "'" + value + "'";
+
+        return value;
+    }
+    public double quoteSmart(double value) {
+        return value;
+    }
+    public int quoteSmart(int value) {
+        return value;
     }
 
-    //8.Count
+    /* 08 Insert data ------------------------------------------------------------ */
+    public void insert(String table, String fields, String values){
+
+        try {
+            db.execSQL("INSERT INTO " + table +  "(" + fields + ") VALUES (" + values + ")");
+        }
+        catch(SQLiteException e){
+            System.out.println("Insert error: " + e.toString());
+        }
+    }
+
+    /* 09 Count ------------------------------------------------------------------ */
     public int count(String table)
     {
-        Cursor mCount = db.rawQuery("SELECT COUNT(*) FROM " + table + "", null);
-        mCount.moveToFirst();
-        int count= mCount.getInt(0);
-        mCount.close();
-        return count;
+        try {
+            Cursor mCount = db.rawQuery("SELECT COUNT(*) FROM " + table + "", null);
+            mCount.moveToFirst();
+            int count = mCount.getInt(0);
+            mCount.close();
+            return count;
+        }
+        catch(SQLiteException e){
+            return -1;
+        }
+
     }
+
+
+
 }
--- app/src/main/java/com/example/myapplication/SignUp.java
+++ app/src/main/java/com/example/myapplication/SignUp.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.view.View;
@@ -12,19 +13,24 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUp extends AppCompatActivity {
 
-    // Variables
+
+    /* Variables */
     private String[] arraySpinnerDOBDay = new String[31];
     private String[] arraySpinnerDOBYear = new String[100];
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
+
 
         /* Fill numbers for date of birth days */
         int human_counter = 0;
@@ -55,6 +61,9 @@ public class SignUp extends AppCompatActivity {
                 android.R.layout.simple_spinner_item, arraySpinnerDOBYear);
         spinnerDOBYear.setAdapter(adapterYear);
 
+
+
+
         /* Hide error icon and message */
         ImageView imageViewError = (ImageView)findViewById(R.id.imageViewError);
         imageViewError.setVisibility(View.GONE);
@@ -65,6 +74,7 @@ public class SignUp extends AppCompatActivity {
         /* Hide icnhes field */
         EditText editTextHeightInches = (EditText)findViewById(R.id.editTextHeightInches);
         editTextHeightInches.setVisibility(View.GONE);
+
 
         /* Listener Mesurment spinner */
         Spinner spinnerMesurment = (Spinner)findViewById(R.id.spinnerMesurment);
@@ -80,8 +90,11 @@ public class SignUp extends AppCompatActivity {
         });
 
 
+
+
+
         /* Listener buttonSignUp */
-        Button buttonSignUp = findViewById(R.id.buttonSignUp);
+        Button buttonSignUp = (Button)findViewById(R.id.buttonSignUp);
         buttonSignUp.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -89,7 +102,8 @@ public class SignUp extends AppCompatActivity {
             }
         });
 
-    }
+
+    } // protected void onCreate
 
     /*- Mesurment changed ------------------------------------------ */
     public void mesurmentChanged() {
@@ -99,23 +113,102 @@ public class SignUp extends AppCompatActivity {
         String stringMesurment = spinnerMesurment.getSelectedItem().toString();
 
 
+        EditText editTextHeightCm = (EditText)findViewById(R.id.editTextHeightCm);
         EditText editTextHeightInches = (EditText)findViewById(R.id.editTextHeightInches);
+        String stringHeightCm = editTextHeightCm.getText().toString();
+        String stringHeightInches = editTextHeightInches.getText().toString();
+
+        double heightCm = 0;
+        double heightFeet = 0;
+        double heightInches = 0;
+
         TextView textViewCm = (TextView)findViewById(R.id.textViewCm);
         TextView textViewKg = (TextView)findViewById(R.id.textViewKg);
 
-        if(stringMesurment.startsWith("M")){
-            // Metric
+        if(stringMesurment.startsWith("I")){
+            // Imperial
             editTextHeightInches.setVisibility(View.VISIBLE);
             textViewCm.setText("feet and inches");
             textViewKg.setText("pound");
-        }
+
+            // Find feet
+            try {
+                heightCm = Double.parseDouble(stringHeightCm);
+            }
+            catch(NumberFormatException nfe) {
+
+            }
+            if(heightCm != 0){
+                // Convert CM into feet
+                // feet = cm * 0.3937008)/12
+                heightFeet = (heightCm * 0.3937008)/12;
+                // heightFeet = Math.round(heightFeet);
+                int intHeightFeet = (int) heightFeet;
+
+                editTextHeightCm.setText("" + intHeightFeet);
+
+            }
+
+        } // if(stringMesurment.startsWith("I")){
         else{
-            // Imperial
+            // Metric
             editTextHeightInches.setVisibility(View.GONE);
             textViewCm.setText("cm");
             textViewKg.setText("kg");
+
+            // Change feet and inches to cm
+
+            // Convert Feet
+            try {
+                heightFeet = Double.parseDouble(stringHeightCm);
+            }
+            catch(NumberFormatException nfe) {
+
+            }
+
+            // Convert inches
+            try {
+                heightInches = Double.parseDouble(stringHeightInches);
+            }
+            catch(NumberFormatException nfe) {
+
+            }
+
+            // Need to convert, we want to save the number in cm
+            // cm = ((foot * 12) + inches) * 2.54
+            if(heightFeet != 0 && heightInches != 0) {
+                heightCm = ((heightFeet * 12) + heightInches) * 2.54;
+                heightCm = Math.round(heightCm);
+                editTextHeightCm.setText("" + heightCm);
+            }
         }
-    }
+
+
+
+        // Weight
+        EditText editTextWeight = (EditText)findViewById(R.id.editTextWeight);
+        String stringWeight = editTextWeight.getText().toString();
+        double doubleWeight = 0;
+
+        try {
+            doubleWeight = Double.parseDouble(stringWeight);
+        }
+        catch(NumberFormatException nfe) {
+        }
+
+        if(doubleWeight != 0) {
+
+            if (stringMesurment.startsWith("I")) {
+                // kg to punds
+                doubleWeight = Math.round(doubleWeight / 0.45359237);
+            } else {
+                // pounds to kg
+                doubleWeight = Math.round(doubleWeight * 0.45359237);
+            }
+            editTextWeight.setText("" + doubleWeight);
+        }
+
+    } // public voild messuredChanged
 
     /*- Sign up Submit ---------------------------------------------- */
     public void signUpSubmit() {
@@ -211,18 +304,140 @@ public class SignUp extends AppCompatActivity {
 
         // Gender
         RadioGroup radioGroupGender = (RadioGroup)findViewById(R.id.radioGroupGender);
-        int selectedId = radioGroupGender.getCheckedRadioButtonId(); // get selected radio button from radioGroup
-        RadioButton radioButtonGender = (RadioButton) findViewById(selectedId); // find the radiobutton by returned id
+        int radioButtonID = radioGroupGender.getCheckedRadioButtonId(); // get selected radio button from radioGroup
+        View radioButtonGender = radioGroupGender.findViewById(radioButtonID);
+        int position = radioGroupGender.indexOfChild(radioButtonGender); // If you want position of Radiobutton
+
+        String stringGender = "";
+        if(position == 0){
+            stringGender = "male";
+        }
+        else{
+            stringGender = "female";
+        }
 
 
 
 
+        /* Height */
+        EditText editTextHeightCm = (EditText)findViewById(R.id.editTextHeightCm);
+        EditText editTextHeightInches = (EditText)findViewById(R.id.editTextHeightInches);
+        String stringHeightCm = editTextHeightCm.getText().toString();
+        String stringHeightInches = editTextHeightInches.getText().toString();
+
+        double heightCm = 0;
+        double heightFeet = 0;
+        double heightInches = 0;
+        boolean metric = true;
+
+        // Metric or imperial?
+        Spinner spinnerMesurment = (Spinner)findViewById(R.id.spinnerMesurment);
+        String stringMesurment = spinnerMesurment.getSelectedItem().toString();
+
+        int intMesurment = spinnerMesurment.getSelectedItemPosition();
+        if(intMesurment == 0){
+            stringMesurment = "metric";
+        }
+        else{
+            stringMesurment = "imperial";
+            metric = false;
+        }
+
+        if(metric == true) {
+
+            // Convert CM
+            try {
+                heightCm = Double.parseDouble(stringHeightCm);
+                heightCm = Math.round(heightCm);
+            }
+            catch(NumberFormatException nfe) {
+                errorMessage = "Height (cm) has to be a number.";
+            }
+        }
+        else {
+
+            // Convert Feet
+            try {
+                heightFeet = Double.parseDouble(stringHeightCm);
+            }
+            catch(NumberFormatException nfe) {
+                errorMessage = "Height (feet) has to be a number.";
+            }
+
+            // Convert inches
+            try {
+                heightInches = Double.parseDouble(stringHeightInches);
+            }
+            catch(NumberFormatException nfe) {
+                errorMessage = "Height (inches) has to be a number.";
+            }
+
+            // Need to convert, we want to save the number in cm
+            // cm = ((foot * 12) + inches) * 2.54
+            heightCm = ((heightFeet * 12) + heightInches) * 2.54;
+            heightCm = Math.round(heightCm);
+        }
+
+        // Weight
+        EditText editTextWeight = (EditText)findViewById(R.id.editTextWeight);
+        String stringWeight = editTextWeight.getText().toString();
+        double doubleWeight = 0;
+        try {
+            doubleWeight = Double.parseDouble(stringWeight);
+        }
+        catch(NumberFormatException nfe) {
+            errorMessage = "Weight has to be a number.";
+        }
+        if(metric == true) {
+        }
+        else{
+            // Imperial
+            // Pound to kg
+            doubleWeight = Math.round(doubleWeight*0.45359237);
+        }
+
+
+        // Activity level
+        Spinner spinnerActivityLevel = (Spinner)findViewById(R.id.spinnerActivityLevel);
+        //  0: Little to no exercise
+        // 1: Light exercise (1–3 days per week)
+        // 2: Moderate exercise (3–5 days per week)
+        // 3: Heavy exercise (6–7 days per week)
+        // 4: Very heavy exercise (twice per day, extra heavy workouts)
+        int intActivityLevel = spinnerActivityLevel.getSelectedItemPosition();
 
         // Error handling
         if(errorMessage.isEmpty()){
             // Put data into database
             imageViewError.setVisibility(View.GONE);
             textViewErrorMessage.setVisibility(View.GONE);
+
+
+            // Insert into database
+            DBAdapter db = new DBAdapter(this);
+            db.open();
+
+
+            // Quote smart
+            String stringEmailSQL = db.quoteSmart(stringEmail);
+            String dateOfBirthSQL = db.quoteSmart(dateOfBirth);
+            String stringGenderSQL = db.quoteSmart(stringGender);
+            double heightCmSQL = db.quoteSmart(heightCm);
+            int intActivityLevelSQL = db.quoteSmart(intActivityLevel);
+            double doubleWeightSQL = db.quoteSmart(doubleWeight);
+            String stringMesurmentSQL = db.quoteSmart(stringMesurment);
+
+            String stringInput = "NULL, " + stringEmailSQL + "," + dateOfBirthSQL + "," + stringGenderSQL + "," + heightCmSQL + "," + intActivityLevelSQL + "," + doubleWeightSQL + "," + stringMesurmentSQL;
+            db.insert("users",
+                    "user_id, user_email, user_dob, user_gender, user_height, user_activity_level, user_weight, user_mesurment",
+                    stringInput);
+
+            db.close();
+
+            // Move user back to MainActivity
+            Intent i = new Intent(SignUp.this, MainActivity.class);
+            startActivity(i);
+
         }
         else {
             // There is error
@@ -231,4 +446,5 @@ public class SignUp extends AppCompatActivity {
             textViewErrorMessage.setVisibility(View.VISIBLE);
         }
     }
-}
+
+} // public class SignUp
